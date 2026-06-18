5a64c38eac6ccc46d5791851e1ae9e7f0129a52b
dev_mong <74331917+KIMDAHYUN98@users.noreply.github.com>
2026-06-18 19:48:48 +0900

    Update 삼각형의 완성조건 （1）.js

--- 프로그래머스/0/120889. 삼각형의 완성조건 （1）/삼각형의 완성조건 （1）.js
+++ 프로그래머스/0/120889. 삼각형의 완성조건 （1）/삼각형의 완성조건 （1）.js
@@ -1,6 +1,14 @@
+
+// 방법 1
 function solution(sides) {
     const large = Math.max(...sides);
     const largeIdx = sides.indexOf(large);
     
     return sides.filter((cur, i) => i !== largeIdx).reduce((acc, cur) => acc + cur > large ? 1 : 2)
+}
+
+// 방법 2, sort 방식으로 오름차순으로 정렬 후 삼항 연산자로 비교
+function solution(sides) {
+    sides = sides.sort((a, b) => a - b); // 오름차순으로 정렬
+    return side[0] + side[1] > side[2] ? 1 : 2;
 }
